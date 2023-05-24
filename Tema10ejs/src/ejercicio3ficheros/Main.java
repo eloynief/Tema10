87da3267fd2d6d7fb59c9979246d565ce8744c53
enieves <enieves@106-02.info2>
2023-05-24 14:38:03 +0200

    terminar ya

--- Tema10ejs/src/ejercicio3ficheros/Main.java
+++ Tema10ejs/src/ejercicio3ficheros/Main.java
@@ -19,10 +19,30 @@ public class Main {
 		try {
 			//sirve para poder escribir en el txt
 			bw = new BufferedWriter(new FileWriter("src\\ejercicio8\\ej8"));
+			//sirve para recoger de readline
+			
+			String linea="";
+			String lineas[]= {};
+			
+			//readline te salta a la siguiente linea
+			
+			
+			
+			
+			
+			
+			
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		//colocar datos
+		nombre=sc.next();
+		edad=sc.nextInt();
+		altura=sc.nextDouble();
+		
+		
 		
 		
 		
